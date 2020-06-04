4c3f6916a2e576221972cc41e09fc5d647d289cb
unknown <wangcj@sinodsp.com>
2020-06-04 15:49:20 +0800

    new project(Counter) add

---
+++ Practices/Counter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter c1 = new Counter();
+            Counter c2 = c1;
+            c1++;
+            Console.WriteLine("c1:" + c1.Count);
+            c1++;
+            Console.WriteLine("c1:" + c1.Count);
+            c1 = c1.GetNextValue();
+            Console.WriteLine("c1:" + c1.Count);
+            c2++;
+            Console.WriteLine("c2:" + c2.Count);
+            c1++;
+            Console.WriteLine("c1:" + c1.Count);
+            Console.ReadLine();
+        }
+    }
+}
