a746f9a29b965b24fe8eb4fdad6e36b428fc4122
unknown <weil@cd-notepad1.sinodsp.com>
2020-06-11 10:27:19 +0800

    Create the Practices in VS2008

---
+++ Practices_2008/Ch3_Interfaces/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Ch3_Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            Console.WriteLine("Folder path:" + path);
+            string[] files = Directory.GetFiles(path);
+            var comparer = new LengthCompare();
+            Array.Sort(files, comparer);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadLine();
+        }
+
+        private class LengthCompare : IComparer<string>
+        {
+            public int Compare(string x, string y)
+            {
+                int diff = x.Length - y.Length;
+                return diff == 0 ? x.CompareTo(y) : diff;
+            }
+        }
+    }
+}
