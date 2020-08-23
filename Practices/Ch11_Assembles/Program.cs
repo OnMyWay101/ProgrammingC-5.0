3ebff2cef9801e12b3fad4e50f1f28ac8e6fabe5
WangChangJie <625504945@qq.com>
2020-08-23 15:54:46 +0800

    Create the chptear 11

---
+++ Practices/Ch11_Assembles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace System
+{
+    class String
+    {
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] agrs)
+    {
+        System.String s1 = null;
+        ShowStaticTypeNameAndAssembly(s1);
+        String s2 = null;
+        ShowStaticTypeNameAndAssembly(s2);
+        ShowStaticTypeNameAndAssembly("String literal");
+        ShowStaticTypeNameAndAssembly(Environment.OSVersion.VersionString);
+        Console.ReadLine();
+    }
+
+    static void ShowStaticTypeNameAndAssembly<T>(T value)
+    {
+        Type t = typeof(T);
+        Console.WriteLine("Type:{0},Assembly:{1}", t.FullName, t.Assembly.FullName);
+    }
+}
