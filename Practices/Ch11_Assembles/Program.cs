5a2bcfd8429e8668f95e215d4de7b2913d1edc20
WangChangJie <625504945@qq.com>
2020-09-27 08:32:39 +0800

    Ch16 project create

--- Practices/Ch11_Assembles/Program.cs
+++ Practices/Ch11_Assembles/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Resources;
 
 namespace System
 {
@@ -12,18 +13,33 @@ class Program
 {
     static void Main(string[] agrs)
     {
+        //Example_Name();
+        Example_Resource();
+        Console.ReadLine();
+    }
+
+
+    static void Example_Name()
+    {
         System.String s1 = null;
         ShowStaticTypeNameAndAssembly(s1);
         String s2 = null;
         ShowStaticTypeNameAndAssembly(s2);
         ShowStaticTypeNameAndAssembly("String literal");
         ShowStaticTypeNameAndAssembly(Environment.OSVersion.VersionString);
-        Console.ReadLine();
     }
 
     static void ShowStaticTypeNameAndAssembly<T>(T value)
     {
         Type t = typeof(T);
         Console.WriteLine("Type:{0},Assembly:{1}", t.FullName, t.Assembly.FullName);
+    }
+
+    static void Example_Resource()
+    {
+        var resource = new ResourceManager("Ch11_Assembles.MyResource", typeof(Program).Assembly);
+        string colText = resource.GetString("ColString");
+
+        Console.WriteLine("Add the color string:" + colText);
     }
 }
